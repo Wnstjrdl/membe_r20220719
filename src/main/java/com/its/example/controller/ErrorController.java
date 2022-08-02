0a3e72f7f5ee23f07b52e96f5068e634c52753b2
wnstjrdl <wnstjr490@naver.com>
2022-08-02 11:25:01 +0900

    에러페이지 추가

---
+++ src/main/java/com/its/example/controller/ErrorController.java
@@ -0,0 +1,23 @@
+package com.its.example.controller;
+
+import com.its.example.errorhandling.exception.ForbiddenException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.awt.*;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/internalerror")
+
+    public  void  internalerror(){
+        throw  new RuntimeException("500 Internal Error !!");
+    }
+
+    @GetMapping("/forbidden")
+    public  void  forbidden(){
+        throw  new ForbiddenException("403 Forbidden !!");
+    }
+
+}
